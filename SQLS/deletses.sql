dad6e8f0b1bc4a21c9e8cb72fe35195214ae2cb8
Lia <dayin.9905@gmail.com>
2025-11-20 16:29:44 +0900

    first commit

---
+++ SQLS/deletses.sql
@@ -0,0 +1,5 @@
+-- DELETE Syntax
+-- DELETE FROM table_name WHERE condition;
+
+DELETE FROM persons WHERE personid = 3;
+
---
+++ SQLS/insert_into_values.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO table_name (column1, column2, column3, ...)
+-- VALUES (value1, value2, value3, ...);
+
+INSERT INTO persons (personid, lastname, firstname, address, city)
+-- VALUES (1, '정', '다인', '서울시', '마포');
+-- VALUES (2, '정', '다인', '서울시', '마포');
+VALUES (3, '정', '다인', '서울시', '마포');

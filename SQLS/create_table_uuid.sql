f53495b43c5c8d686132d570dc4b45af9b4d2b84
Lia <dayin.9905@gmail.com>
2025-11-21 16:16:35 +0900

    00

--- SQLS/create_table_uuid.sql
+++ SQLS/create_table_uuid.sql
@@ -6,6 +6,7 @@ CREATE TABLE users_uuid_name (
 );
 
 INSERT INTO users_uuid_name (name) VALUES ('Alice');
+INSERT INTO users_uuid_name (name) VALUES ('Bob');
 
 SELECT * FROM users_uuid_name;
 
@@ -15,3 +16,5 @@ UPDATE users_uuid_name
 SET name = 'UpdatedName'
 WHERE id_name = '8cbefa31-ba2e-45fe-a366-2ee6599ab55e';
 
+DELETE FROM users_uuid_name
+WHERE id_name = 'dcf741d0-aa8b-4c48-8fe3-a907f7f4c0a3'; 
--- codes/postgres_connections.py
+++ codes/postgres_connections.py
@@ -21,9 +21,19 @@ print("PostgreSQL 데이터베이스에 성공적으로 연결되었습니다.")
 
 with conn.cursor() as cursor :
     # cursor.execute("INSERT INTO users_uuid_name (name) VALUES ('from code');") # 데이터 삽입
-    cursor.execute("""UPDATE users_uuid_name
-                    SET name = 'code name'
-                     WHERE id_name = '8cbefa31-ba2e-45fe-a366-2ee6599ab55e';""") # 데이터 삽입
+
+    # cursor.execute("""UPDATE users_uuid_name
+    #                 SET name = 'code name'
+    #                  WHERE id_name = '8cbefa31-ba2e-45fe-a366-2ee6599ab55e';""") # 데이터 수정
+
+    # cursor.execute("""DELETE FROM users_uuid_name
+    #                  WHERE id_name = 'dcf741d0-aa8b-4c48-8fe3-a907f7f4c0a3';""") # 데이터 삭제
+
+    cursor.execute("SELECT name, id_name FROM users_uuid_name;") # 데이터 조회
+    recods = cursor.fetchall() # 조회된 데이터 모두 가져오기
+    for record in recods : # 조회된 데이터 출력 # recodes -> record의 복수형 # row는 list 형태
+        print(record)
+        print(f'{record[0]} : {record[1]}') # 인덱스로 접근 가능
 
 conn.commit() # 완료
 
